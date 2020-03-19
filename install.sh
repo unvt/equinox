8e490275dc9a05218b481509aee4e8f970795e8c
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-20 08:38:25 +0900

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
-task :world do
-s = <<EOS
 sudo true
 sudo apt update
 sudo apt upgrade
-sudo apt install autoconf automake bash curl g++ git h2o htop libtool linux-headers make nginx nodejs npm openssh python ruby ruby-rake sqlite sqlite-dev tmux vim yarn zip zlib-dev 
+sudo apt install autoconf automake bash curl g++ git h2o htop libtool linux-headers make nginx nodejs npm python ruby sqlite tmux vim yarn zip 
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make; make install; cd ..
 rm -rf tippecanoe
 yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
 cd vt-optimizer; npm install; cd ..
-EOS
-  s.split("\n").each {|l|
-    sh l
-  }
-end
 
