10923d7b380624ec866eff2384fc8c80c480d291
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-20 08:58:54 +0900

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt install --allow-change-held-packages autoconf automake bash curl g++ git h2o htop libsqlite3-dev libtool linux-headers make nginx nodejs npm python ruby sqlite tmux vim yarn zip 
 git clone https://github.com/mapbox/tippecanoe
-cd tippecanoe; make; make install; cd ..
+cd tippecanoe; make -j; make install; cd ..
 rm -rf tippecanoe
 yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
