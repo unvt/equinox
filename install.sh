b5f929ceb4643eddb08ed01d72d956782525e0ca
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-20 16:14:17 +0900

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-apt update
-apt -y upgrade
-curl -sL https://deb.nodesource.com/setup_13.x | bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -    
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt install -y autoconf automake git libsqlite3-dev libtool linux-headers nginx nodejs ruby sqlite tmux vim yarn zip 
+sudo apt update
+sudo apt -y upgrade
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install -y autoconf automake git libsqlite3-dev libtool linux-headers nginx nodejs ruby sqlite tmux vim yarn zip 
 git clone https://github.com/mapbox/tippecanoe
-cd tippecanoe; make -j3 LDFLAGS="-latomic"; make install; cd ..
+cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
 rm -rf tippecanoe
-yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
+sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
 cd vt-optimizer; npm install; cd ..
 
