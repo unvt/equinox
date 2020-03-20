2ffd3da57fe97ff9fcc13ebc0a12d192b5b6fdbb
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-20 12:13:35 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # equinox
 UNVT on host OS - experimental
+
+# Installing in a fresh Raspbian Lite
+```
+curl -sL https://unvt.github.io/equinox/install.sh | sudo -E bash -
+```
--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 sudo true
 sudo apt update
-sudo apt upgrade
+sudo apt --allow-change-held-packages upgrade
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt install --allow-change-held-packages autoconf automake bash curl g++ git h2o htop libsqlite3-dev libtool linux-headers make nginx nodejs npm python ruby sqlite tmux vim yarn zip 
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt upgrade
+sudo apt install --allow-change-held-packages autoconf automake git h2o libsqlite3-dev libtool linux-headers nodejs ruby sqlite tmux vim yarn zip 
 git clone https://github.com/mapbox/tippecanoe
-cd tippecanoe; make -j; make install; cd ..
+cd tippecanoe; make -j3 LDFLAGS="-latomic"; make install; cd ..
 rm -rf tippecanoe
 yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
