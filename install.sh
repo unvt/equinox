2b034375f3ed011657aedf83ad6b6556b8466643
FUJIMURA, Hidenori <hfu@users.noreply.github.com>
2020-04-10 13:44:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ sudo apt -y upgrade
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt install -y autoconf automake gdal-bin git libsqlite3-dev libtool linux-headers nginx nodejs osmium-tool ruby sqlite tmux vim yarn zip 
+sudo apt install -y autoconf automake gdal-bin git libsqlite3-dev libtool linux-headers nginx nodejs osmium-tool ruby sqlite3 tmux vim yarn zip 
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
 rm -rf tippecanoe
-sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
+sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @mapbox/vtshaver @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
 cd vt-optimizer; npm install; cd ..
-
+sudo gem install mdless
