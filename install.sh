af9fcf79fe1ccb98eb0f64d4a20db6cb9093014e
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-09-12 14:39:23 +0900

    add xrdp

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y autoconf automake cmake gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool ruby sqlite3 tmux vim yarn zip 
+sudo apt install -y autoconf automake cmake gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool ruby sqlite3 tmux vim xrdp yarn zip 
+sudo service xrdp restart
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
 rm -rf tippecanoe
