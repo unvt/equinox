897215f26e6181213bdd205517d50820ff118385
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2021-07-16 05:01:04 +0900

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,6 @@
 - Switched `code` to the Raspberry Pi OS default.
 - Removed `hocon` and `mdless` gem.
 
+## 2021-07-16
+- Added `python-gdal`.
+
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y autoconf automake cmake code gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel ruby sqlite3 tmux vim xrdp yarn zip 
+sudo apt install -y autoconf automake cmake code gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel python-gdal ruby sqlite3 tmux vim xrdp yarn zip 
 sudo service xrdp restart
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
