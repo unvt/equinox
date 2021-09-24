0926d77fba446a378e833feadcfa2647b248b07e
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2021-09-25 06:58:44 +0900

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 sudo apt update
 sudo apt -y upgrade
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y autoconf automake cmake code gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel pdal python-gdal ruby sqlite3 tmux vim xrdp yarn zip 
+sudo apt install -y autoconf automake cmake code gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel pdal python-gdal ruby sqlite3 tmux unar vim xrdp yarn zip 
 sudo service xrdp restart
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
 rm -rf tippecanoe
 sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer
-cd vt-optimizer; npm install; cd ..
+cd vt-optimizer; yarn; cd ..
 
