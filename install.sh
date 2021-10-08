d65194d20d0694fb23162b74bc94c5c2111f96a5
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2021-10-08 22:05:04 +0900

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
+cd $HOME
 sudo apt update
 sudo apt -y upgrade
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -    
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y autoconf automake cmake code gdal-bin git libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel pdal python-gdal ruby sqlite3 tmux unar vim xrdp yarn zip 
+sudo apt install -y autoconf automake cmake code gdal-bin git gpsd gpsd-clients python-gps libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel pdal python-gdal ruby sqlite3 tmux unar vim xrdp yarn zip 
 sudo service xrdp restart
 git clone https://github.com/mapbox/tippecanoe
 cd tippecanoe; make -j3 LDFLAGS="-latomic"; sudo make install; cd ..
 rm -rf tippecanoe
 sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
-git clone https://github.com/ibesora/vt-optimizer
+git clone https://github.com/ibesora/vt-optimizer.git
 cd vt-optimizer; yarn; cd ..
-
+git clone https://github.com/geolonia/charites.git
+sudo npm install -g npm; cd charites; npm install; npm run build; sudo npm install -g .; cd ..
