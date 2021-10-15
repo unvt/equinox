572fbf3a8c9ed25c611427394f7429acc36c74fb
Hidenori Fujimura <hfu@users.noreply.github.com>
2021-10-16 07:51:14 +0900

    update

--- install.sh
+++ install.sh
@@ -13,5 +13,4 @@ rm -rf tippecanoe
 sudo yarn global add browserify budo hjson pm2 rollup @mapbox/mapbox-gl-style-spec @pushcorn/hocon-parser
 git clone https://github.com/ibesora/vt-optimizer.git
 cd vt-optimizer; yarn; cd ..
-git clone https://github.com/geolonia/charites.git
-sudo npm install -g npm; cd charites; npm install; npm run build; sudo npm install -g .; cd ..
+sudo npm install -g @unvt/charites
