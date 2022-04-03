eff45275ebd2ea8dad66e2f1526ee10c4d351860
Hidenori <hfu@users.noreply.github.com>
2022-04-03 22:37:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cd $HOME
 sudo apt update
 sudo apt -y upgrade
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 sudo apt install -y autoconf automake cmake code gdal-bin git gpsd gpsd-clients python-gps libsqlite3-dev libtool raspberrypi-kernel-headers nginx nodejs osmium-tool parallel pdal python-gdal ruby sqlite3 tmux unar vim xrdp yarn zip 
